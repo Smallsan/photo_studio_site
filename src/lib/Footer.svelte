21bf85b8292e99365e7cedfce4a8efd3ce1bed9e
Kurenairisu <minec323@gmail.com>
2024-07-08 15:23:20 +0800

    created footer

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,16 @@
+<footer class="footer">
+    <div class="content">
+        <p>©2024 Tans' Self Portrait Studio</p>
+    </div>
+</footer>
+
+<style>
+    .footer {
+        background-color: #f5f5f5;
+        text-align: center;
+        padding: 20px 0;
+        position: relative;
+        bottom: 0;
+        width: 100%;
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,35 +1,37 @@
 <script>
-    import { onMount } from 'svelte';
-    import Navbar from "$lib/Navbar.svelte";
-    import '../app.css'
-    onMount(() => {
-        // Dynamically load the AOS script
-        const script = document.createElement('script');
-        script.src = 'https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js';
-        script.onload = () => {
-            // @ts-ignore
-            AOS.init({
-                // AOS options here
-                duration: 800,
-                once: true,
-            });
-        };
-        document.head.appendChild(script);
-    });
-
+	import { onMount } from 'svelte';
+	import Navbar from '$lib/Navbar.svelte';
+	import Footer from '$lib/Footer.svelte';
+	import '../app.css';
+	onMount(() => {
+		// Dynamically load the AOS script
+		const script = document.createElement('script');
+		script.src = 'https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js';
+		script.onload = () => {
+			// @ts-ignore
+			AOS.init({
+				// AOS options here
+				duration: 800,
+				once: true
+			});
+		};
+		document.head.appendChild(script);
+	});
 </script>
 
 <svelte:head>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.css" />
 </svelte:head>
-
-<style>
-    :global(body), :global(html) {
-      margin: 0;
-      padding: 0;
-    }
-  </style>
 
 <Navbar />
 
 <slot />
+<Footer />
+
+<style>
+	:global(body),
+	:global(html) {
+		margin: 0;
+		padding: 0;
+	}
+</style>
